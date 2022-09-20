fab06989fc3b459e6107478889bd97460fa48bf6
Aleksandr Videvich <videvichs@gmail.com>
2022-09-20 16:51:27 +0300

    fixed task3.java

--- lab3/src/Task3.java
+++ lab3/src/Task3.java
@@ -1,13 +1,19 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Task3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String line = input.nextLine();
         input.close();
-        line = line.trim().replaceAll("\\s+", " ");
+        String[] words = line.split(" ");
+        for(int i = 0; i < words.length; ++i) {
+            if(words[i].length() == 1)
+                words[i] = " ";
+        }
+        line = String.join(" ", words).trim().replaceAll("\\s+", " ");
         System.out.println(line);
     }
 }
